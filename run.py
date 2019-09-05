6119d9210faa08acba517f268639a899cadeee83
mireille <mimimireille95@gmail.com>
2019-09-05 06:08:02 -0400

    if conditon

--- run.py
+++ run.py
@@ -55,43 +55,43 @@ def main():
                             l_name = input()
 
                             print("Password ...")
-                            p_number = input()
+                            p_password = input()
 
 
-                            save_contacts(create_contact(f_name,l_name,p_number,e_address)) # create and save new contact.
+                            save_contacts(create_contact(f_name,l_name,p_password)) # create and save new contact.
                             print ('\n')
                             print(f"New Contact {f_name} {l_name} created")
                             print ('\n')
 
                     elif short_code == 'dc':
 
-                            if display_contacts():
-                                    print("Here is a list of all your contacts")
+                            if display_users():
+                                    print("Here is a list of all your users")
                                     print('\n')
 
-                                    for contact in display_contacts():
-                                            print(f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
+                                    for user in display_users():
+                                            print(f"{user.first_name} {user.last_name} .....{contact.password}")
 
                                     print('\n')
                             else:
                                     print('\n')
-                                    print("You dont seem to have any contacts saved yet")
+                                    print("You dont seem to have any account saved yet")
                                     print('\n')
 
                     elif short_code == 'fc':
 
                             print("Enter the number you want to search for")
 
-                            search_number = input()
-                            if check_existing_contacts(search_number):
-                                    search_contact = find_contact(search_number)
-                                    print(f"{search_contact.first_name} {search_contact.last_name}")
+                            search_first_name = input()
+                            if check_existing_users(search_first_name):
+                                    search_user = find_user(search_first_name)
+                                    print(f"{search_user.first_name} {search_user.last_name}")
                                     print('-' * 20)
 
-                                    print(f"Phone number.......{search_contact.phone_number}")
-                                    print(f"Email address.......{search_contact.email}")
+                                    print(f"Password.......{search_contact.password}")
+                                   
                             else:
-                                    print("That contact does not exist")
+                                    print("That user does not exist")
 
                     elif short_code == "ex":
                             print("Bye .......")
