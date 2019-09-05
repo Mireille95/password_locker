25cf4a4d9b4c46c9d03f11d7341c44a113af6ff6
mireille <mimimireille95@gmail.com>
2019-09-05 10:10:55 -0400

    newn line

--- run.py
+++ run.py
@@ -83,64 +83,95 @@ def main():
          print(' ')
          print("-"*60)
          print('Use these codes to navigate: \n ca-Create an Account \n li-Log In \n ex-Exit')
-	 short_code = input('Enter a choice: ').lower().strip()
-	 if short_code == 'ex':
-		break
+         short_code = input('Enter a choice: ').lower().strip()
+         if short_code == 'ex':
+                 break
+         elif short_code == 'ca':
+                 print("-"*60)
+                 print(' ')
+                 print('To create a new account')
+                 first_name = input('Enter your First name - ').strip()
+                 last_name = input('Enter your last name - ').strip()
+                 save_user(create_user(first_name,last_name,password))
+                 print(" ")
+                 print(f'New Account Created for: {first_name} {last_name} using password: {password}')
+         
+         elif short_code == 'li':
+		 print("-"*60)
+		 print(' ')
+		 print('To login, enter your account details:')
+		 user_name = input('Enter your first name - ').strip()
+		 password = str(input('Enter your password - '))
+		 user_exists = verify_user(user_name,password)
+		if user_exists == user_name:
+                        print(" ")
+			print(f'Welcome {user_name}. Please choose an option to continue.')
+			print(' ')
+			while True:
+					print("-"*60)
+					print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
+					short_code = input('Enter a choice: ').lower().strip()
+					print("-"*60)
+					if short_code == 'ex':
+						print(" ")
+						print(f'Goodbye {user_name}')
+						break
+					elif short_code == 'cc':
+						print(' ')
+						print('Enter your credential details:')
+						site_name = input('Enter the site\'s name- ').strip()
+						account_name = input('Enter your account\'s name - ').strip()
+						while True:
+							print(' ')
+							print("-"*60)
+							print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
+							psw_choice = input('Enter an option: ').lower().strip()
+							print("-"*60)
+							if psw_choice == 'ep':
+								print(" ")
+								password = input('Enter your password: ').strip()
+								break
+							elif psw_choice == 'gp':
+								password = generate_password()
+								break
+							elif psw_choice == 'ex':
+								break
+							else:
+								print('Oops! Wrong option entered. Try again.')
+						save_credential(create_credential(user_name,site_name,account_name,password))
+						print(' ')
+						print(f'Credential Created: Site Name: {site_name} - Account Name: {account_name} - Password: {password}')
+						print(' ')
+					elif short_code == 'dc':
+						print(' ')
+						if display_credentials(user_name):
+							print('Here is a list of all your credentials')
+							print(' ')
+							for credential in display_credentials(user_name):
+								print(f'Site Name: {credential.site_name} - Account Name: {credential.account_name} - Password: {credential.password}')
+							print(' ')	
+						else:
+							print(' ')
+							print("You don't seem to have any credentials saved yet")
+							print(' ')
+					elif short_code == 'copy':
+						print(' ')
+						chosen_site = input('Enter the site name for the credential password to copy: ')
+						copy_credential(chosen_site)
+						print('')
+					else:
+						print('Oops! Wrong option entered. Try again.')
 
-        elif short_code == 'cc':
-                            print("New User")
-                            print("-"*10)
+			else: 
+				print(' ')
+				print('Oops! Wrong details entered. Try again or Create an Account.')		
+		
+		else:
+			print("-"*60)
+			print(' ')
+			print('Oops! Wrong option entered. Try again.')
+				
 
-                            print ("First name ....")
-                            f_name = input()
-
-                            print("Last name ...")
-                            l_name = input()
-
-                            print("Password ...")
-                            p_password = input()
-
-
-                            save_users(create_user(f_name,l_name,p_password)) # create and save new contact.
-                            print ('\n')
-                            print(f"New User {f_name} {l_name} created")
-                            print ('\n')
-
-                    elif short_code == 'dc':
-
-                            if display_users():
-                                    print("Here is a list of all your users")
-                                    print('\n')
-
-                                    for user in display_users():
-                                            print(f"{user.first_name} {user.last_name} .....{user.password}")
-
-                                    print('\n')
-                            else:
-                                    print('\n')
-                                    print("You dont seem to have any account saved yet")
-                                    print('\n')
-
-                    elif short_code == 'fc':
-
-                            print("Enter the name you want to search for")
-
-                            search_first_name = input()
-                            if check_existing_users(search_first_name):
-                                    search_user = find_user(search_first_name)
-                                    print(f"{search_user.first_name} {search_user.last_name}")
-                                    print('-' * 20)
-
-                                    print(f"Password.......{search_user.password}")
-                                   
-                            else:
-                                    print("That user does not exist")
-
-                    elif short_code == "ex":
-                            print("Bye .......")
-                            break
-                    else:
-                            print("I really didn't get that. Please use the short codes")
 
 if __name__ == '__main__':
 
