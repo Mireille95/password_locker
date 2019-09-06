6855cfed3fd16794174f7ba348affec8dc10b3bc
mireille <mimimireille95@gmail.com>
2019-09-06 04:52:15 -0400

    generate  add

--- user.py
+++ user.py
@@ -1,146 +1,226 @@
 
+import pyperclip
 import random
 import string
 
 
+global users_list 
 class User:
-    """
-    Class that generates new instances of users.
-    """
+	'''
+	Class to create user accounts and save their information
+	'''
+	
+	users_list = []
+	def __init__(self,first_name,last_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
 
-    user_list = [] # Empty contact list
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
 
-    def __init__(self,first_name,last_name,password):
+	def save_user(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		User.users_list.append(self)
 
-      # docstring removed for simplicity
+class Credential:
+	'''
+	Class to create  account credentials, generate passwords and save their information
+	'''
+	
+	credentials_list =[]
+	user_credentials_list = []
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.password = password
-       
+	@classmethod
+	def check_user(cls,first_name,password):
+		'''
+		Method that checks if the name and password entered match entries in the users_list
+		'''
+		current_user = ''
+		for user in User.users_list:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
+        
+        
+    
+	def save_credentials(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		# global users_list
+		Credential.credentials_list.append(self)
+	
 
-    def save_user(self):
+	def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+		'''
+		Function to generate an 8 character password for a credential
+		'''
+		gen_pass=''.join(random.choice(char) for _ in range(size))
+		return gen_pass
 
-        '''
-        save_user method saves user objects into user_list
-        '''
+	def __init__(self,user_name,site_name,account_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
 
-        User.user_list.append(self)
+		# instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
 
-    def delete_user(self):
 
-        '''
-        delete_user method deletes a saved user from the user_list
-        '''
+	@classmethod
+	def display_credentials(cls,user_name):
+		'''
+		Class method to display the list of credentials saved
+		'''
+		user_credentials_list = []
 
-        User.user_list.remove(self)
+		for credential in cls.credentials_list:
+			if credential.user_name == user_name:
+				user_credentials_list.append(credential)
+		return user_credentials_list
+				
 
-    # @classmethod
-    # def find_by_first_name(cls,first_name):
-    #     '''
-    #     Method that takes in a name and returns a contact that matches that name.
+	
+	@classmethod
+	def find_by_site_name(cls, site_name):
+		'''
+		Method that takes in a site_name and returns a credential that matches that site_name.
+		'''
+		for credential in cls.credentials_list:
+			if credential.site_name == site_name:
+				return credential
 
-    #     Args:
-    #         number: name to search for
-    #     Returns :
-    #         user of person that matches the name.
-    #     '''
+	@classmethod
+	def copy_credential(cls,site_name):
+		'''
+		Class method that copies a credential's info after the credential's site name is entered
+		'''
+		find_credential = Credential.find_by_site_name(site_name)
+		return pyperclip.copy(find_credential.password)
 
-    #     for user in cls.user_list:
-    #         if user.first_name == first_name:
-    #             return user
-    # @classmethod
-    # def user_exist(cls,first_name,password):
-    #     '''
-    #     Method that checks if a user exists from the user list.
-    #     Args:
-    #         number: first_name to search if it exists
-    #     Returns :
-    #         Boolean: True or false depending if the user exists
-    #     '''
-    #     for user in cls.user_list:
-    #         if user.first_name == first_name and user.password == password:
-    #                 return True
 
-    #     return False
-    # @classmethod
-    # def display_users(cls):
-    #     '''
-    #     method that returns the user list
-    #     '''
-    #     return cls.user_list
 
-    # @classmethod
-    # def copy_password(cls,first_name):
-    #     user_found = User.find_by_first_name(first_name)
-    #     pyperclip.copy(user_found.password)
+#     def delete_user(self):
+
+#         '''
+#         delete_user method deletes a saved user from the user_list
+#         '''
+
+#         User.user_list.remove(self)
+
+#     # @classmethod
+#     # def find_by_first_name(cls,first_name):
+#     #     '''
+#     #     Method that takes in a name and returns a contact that matches that name.
+
+#     #     Args:
+#     #         number: name to search for
+#     #     Returns :
+#     #         user of person that matches the name.
+#     #     '''
+
+#     #     for user in cls.user_list:
+#     #         if user.first_name == first_name:
+#     #             return user
+#     # @classmethod
+#     # def user_exist(cls,first_name,password):
+#     #     '''
+#     #     Method that checks if a user exists from the user list.
+#     #     Args:
+#     #         number: first_name to search if it exists
+#     #     Returns :
+#     #         Boolean: True or false depending if the user exists
+#     #     '''
+#     #     for user in cls.user_list:
+#     #         if user.first_name == first_name and user.password == password:
+#     #                 return True
+
+#     #     return False
+#     # @classmethod
+#     # def display_users(cls):
+#     #     '''
+#     #     method that returns the user list
+#     #     '''
+#     #     return cls.user_list
+
+#     # @classmethod
+#     # def copy_password(cls,first_name):
+#     #     user_found = User.find_by_first_name(first_name)
+#     #     pyperclip.copy(user_found.password)
       
 
 
 
-class Credential:
-    '''
-    class to create account credintials, generate password and save the info
-    '''
-    credentials_list =  []
-    user_credentials_list = []
+# class Credential:
+#     '''
+#     class to create account credintials, generate password and save the info
+#     '''
+#     credentials_list =  []
+#     user_credentials_list = []
 
-    @classmethod
-    def check_user(cls,first_name,password):
-        '''
-        method that checks if the name and passwword entered match
-        '''
-        current_user = ''
-        for user in User.user_list:
-            if (user.first_name == first_name and user.password == password):
-                current_user = user.first_name
-        return current_user
+#     @classmethod
+#     def check_user(cls,first_name,password):
+#         '''
+#         method that checks if the name and passwword entered match
+#         '''
+#         current_user = ''
+#         for user in User.user_list:
+#             if (user.first_name == first_name and user.password == password):
+#                 current_user = user.first_name
+#         return current_user
 
-    def __init__(self,user_name,site_name,account_name,password):
-        '''
-        method to define the properties for each user object will hold
-        '''
+#     def __init__(self,user_name,site_name,account_name,password):
+#         '''
+#         method to define the properties for each user object will hold
+#         '''
 
-        # instance variable
-        self.user_name = user_name
-        self.site_name = site_name
-        self.account_name = account_name
-        self.password =  password
+#         # instance variable
+#         self.user_name = user_name
+#         self.site_name = site_name
+#         self.account_name = account_name
+#         self.password =  password
 
-    def save_credentials(self):
-        '''
-        function to save a new created user insatnce
-        '''
+#     def save_credentials(self):
+#         '''
+#         function to save a new created user insatnce
+#         '''
 
-        Credential.credentials_list.append(self)
+#         Credential.credentials_list.append(self)
 
-    def generate_password(size=8,char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
-        '''
-        function to generate an 8 character password
-        '''
-        gen_pass =''.join(random.choice(char) for _ in range(size))
+#     def generate_password(size=8,char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+#         '''
+#         function to generate an 8 character password
+#         '''
+#         gen_pass =''.join(random.choice(char) for _ in range(size))
 
-        return gen_pass
+#         return gen_pass
 
-    @classmethod
-    def display_credentials(cls,user_name):
-        '''
-        class method to display the list of credentials saved in 
-        '''
+#     @classmethod
+#     def display_credentials(cls,user_name):
+#         '''
+#         class method to display the list of credentials saved in 
+#         '''
 
-        user_credentials_list = []
-        for credential in cls.credentials_list:
-            if credential.user_name == user_name:
-                user_credentials_list.append(credential)
-        return user_credentials_list
+#         user_credentials_list = []
+#         for credential in cls.credentials_list:
+#             if credential.user_name == user_name:
+#                 user_credentials_list.append(credential)
+#         return user_credentials_list
 
-    def delete_credential(self):
+#     def delete_credential(self):
 
-        '''
-        delete_user method deletes a saved user from the user_list
-        '''
+#         '''
+#         delete_user method deletes a saved user from the user_list
+#         '''
 
-        Credential.credential_list.remove(self)
+#         Credential.credential_list.remove(self)
 
 
         
--- user_test.py
+++ user_test.py
@@ -1,220 +1,294 @@
 import unittest # Importing the unittest module
 import pyperclip
 
-
-
-from user import User,Credential # Importing the contact class
-f = open('test.txt', 'r')
-
+from user import User,Credential
 
 class TestUser(unittest.TestCase):
+	'''
+	Test class that defines test cases for the user class behaviours.
 
-    '''
-    Test class that defines test cases for the user class behaviours.
-
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-    '''
-
-    def setUp(self):
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_user = User("James","Muriuki","pawsd12") # create contact object
-
-
-    def test_init(self):
-        '''
-        test_init test case to test if the object is initialized properly
-        '''
-
-        self.assertEqual(self.new_user.first_name,"James")
-        self.assertEqual(self.new_user.last_name,"Muriuki")
-        self.assertEqual(self.new_user.password,"pawsd12")
-        
-
-    def test_save_user(self):
-        '''
-        test_save_user test case to test if the user object is saved into
-         the user list
-        '''
-        self.new_user.save_user() # saving the new user
-        self.assertEqual(len(User.user_list),1)
-    def tearDown(self):
-        
-        '''
-		Function to clear the credentials list after every test
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def setUp(self):
 		'''
-        User.users_list = []
+		Function to create a user account before each test
+		'''
+		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
 
-    # def test_save_multiple_user(self):
-    #         '''
-    #         test_save_multiple_user to check if we can save multiple user
-    #         objects to our user_list
-    #         '''
-    #         self.new_user.save_user()
-    #         test_user = User("Test","user","0712345678") # new contact
-    #         test_user.save_user()
-    #         self.assertEqual(len(User.user_list),2)
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of user instances is properly done
+		'''
+		self.assertEqual(self.new_user.first_name,'Mary')
+		self.assertEqual(self.new_user.last_name,'Ng\'ang\'a')
+		self.assertEqual(self.new_user.password,'pswd100')
 
-    def test_delete_user(self):
-            '''
-            test_delete_user to test if we can remove a user from our user list
-            '''
-            self.new_user.save_user()
-            test_user = User("Test","user","0712345678") # new user
-            test_user.save_user()
-
-            self.new_user.delete_user()# Deleting a user object
-            self.assertEqual(len(User.user_list),1)
-
-
-    # def test_find_user_by_first_name(self):
-    #     '''
-    #     test to check if we can find a user by phone number and display information
-    #     '''
-
-    #     self.new_user.save_user()
-    #     test_user = User("Test","user","0712345678") # new user
-    #     test_user.save_user()
-
-    #     found_user = User.find_by_first_name("Test")
-
-    #     self.assertEqual(found_user.password,test_user.password)
-
- 
-
-    def test_display_all_users(self):
-        '''
-        method that returns a list of all users saved
-        '''
-
-        self.assertEqual(User.display_users(),User.user_list)
-
- 
-
+	def test_save_user(self):
+		'''
+		Test to check if the new users info is saved into the users list
+		'''
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),1)
 
 class TestCredentials(unittest.TestCase):
-    '''
-    test class that defines a test cases dor the credentials class behaviors
-    
-    Args: 
-    unittest.TestCase: helps in creating test cases
-    '''
-    
-    def  test_check_user(self):
-        '''
-        function to test whether the login in function checuser woks as expected
-        '''
+	'''
+	Test class that defines test cases for the credentials class behaviours.
 
-        self.new_user= User('Mimi','mireille','pswd100')
-        self.new_user.save_user()
-        user2 = User('fanny','mireille,'pswd100')
-        user2.save_user()
-        
-        for user in User.user_list:
-            if user.first_name ==  user2.first_name and user.password == user2.password:
-                current_user = user.first_name
-        return current_user
-        
-        self.assertEqual(current_user,Credential.check_user(user2.first_name,user2.password))
-    
-    def setUp(self):
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def test_check_user(self):
+		'''
+		Function to test whether the login in function check_user works as expected
+		'''
+		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
+		self.new_user.save_user()
+		user2 = User('Ken','Ng\'ang\'a','pswd100')
+		user2.save_user()
 
-        '''
-        function to create an account credentials beforre each test
-        '''
-        self.new_credential = Credential('Mireille','Facebook','Mireille_Uwase','pswd100')
-        
-    def test__init__(self):
-        '''
+		for user in User.users_list:
+			if user.first_name == user2.first_name and user.password == user2.password:
+				current_user = user.first_name
+		return current_user
+
+		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
+
+
+	def setUp(self):
+		'''
+		Function to create an account's credentials before each test
+		'''
+		self.new_credential = Credential('Mary','Facebook','maryjoe','pswd100')
+
+	def test__init__(self):
+		'''
 		Test to if check the initialization/creation of credential instances is properly done
 		'''
-        
-        self.assertEqual(self.new_credential.user_name,'Mireille')
-        self.assertEqual(self.new_credential.site_name,'Facebook')
-        self.assertEqual(self.new_credential.account_name,'Mireille_Uwase')
-        self.assertEqual(self.new_credential.password,'pswd100')
-        
-    def test_save_credentials(self):
-        
-        '''
+		self.assertEqual(self.new_credential.user_name,'Mary')
+		self.assertEqual(self.new_credential.site_name,'Facebook')
+		self.assertEqual(self.new_credential.account_name,'maryjoe')
+		self.assertEqual(self.new_credential.password,'pswd100')
+
+
+	def test_save_credentials(self):
+		'''
 		Test to check if the new credential info is saved into the credentials list
 		'''
-        
-        self.new_credential.save_credentials()
-        
-        facebook = Credential('mireille','Facebook','Mireille_Uwase','0727077616')
-        facebook.save_credentials()
-        self.assertEqual(len(Credential.credentials_list),2)
-        
-    def test_delete_credential(self):
-            '''
-            test_delete_user to test if we can remove a user from our user list
-            '''
-            self.new_credential.save_credentials()
-            test_credential = Credential("Test","0712345678") # new user
-            test_credential.save_credentials()
+		self.new_credential.save_credentials()
+		twitter = Credential('Jane','Twitter','maryjoe','pswd100')
+		twitter.save_credentials()
+		self.assertEqual(len(Credential.credentials_list),2)
 
-            self.new_credential.delete_credential()# Deleting a user object
-            self.assertEqual(len(Credential.credential_list),1)
-
-    def tearDown(self):
-        
-        '''
+	def tearDown(self):
+		'''
 		Function to clear the credentials list after every test
 		'''
-        
-        Credential.credentials_list = []
-        User.users_list = []
-        
-    def test_display_credentials(self):
-        
-        '''
+		Credential.credentials_list = []
+		User.users_list = []
+
+
+	def test_display_credentials(self):
+		'''
 		Test to check if the display_credentials method, displays the correct credentials.
 		'''
-        
-        self.new_credential.save_credentials()
-        facebook = Credential('mireille','Facebook','Mireille_Uwase','0727077616')
-        facebook.save_credentials()
-        github = Credential('mireille','github','Mireille95','Mimi@2019')
-        github.save_credentials()
-        
-        self.assertEqual(len(Credential.display_credentials(facebook.user_name)),2)
-        
-    # def test_find_by_site_name(self):
-    #     '''
-	# 	Test to check if the find_by_site_name method returns the correct credential
-	# 	'''
-        
-    #     self.new_credential.save_credentials()
+		self.new_credential.save_credentials()
+		twitter = Credential('Jane','Twitter','maryjoe','pswd100')
+		twitter.save_credentials()
+		gmail = Credential('Jane','Gmail','maryjoe','pswd200')
+		gmail.save_credentials()
+		self.assertEqual(len(Credential.display_credentials(twitter.user_name)),2)
 
-    #     twitter = Credential('Jane','Twitter','maryjoe','pswd100')
-    #     twitter.save_credentials()
-    #     credential_exists = Credential.find_by_site_name('Twitter')
-    #     self.assertEqual(credential_exists,twitter)
-        
-    # def test_copy_credential(self):
-        
-    #     '''
-	# 	Test to check if the copy a credential method copies the correct credential
-	# 	'''
-        
-    #     self.new_credential.save_credentials()
-    #     twitter = Credential('Jane','Twitter','maryjoe','pswd100')
-    #     twitter.save_credentials()
-    #     find_credential = None
-    #     for credential in Credential.user_credentials_list:
-    #         find_credential =Credential.find_by_site_name(credential.site_name)
-    #     return pyperclip.copy(find_credential.password)
-        
-    #     Credential.copy_credential(self.new_credential.site_name)
-    #     self.assertEqual('pswd100',pyperclip.paste())
-    #     print(pyperclip.paste())
+	def test_find_by_site_name(self):
+		'''
+		Test to check if the find_by_site_name method returns the correct credential
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Jane','Twitter','maryjoe','pswd100')
+		twitter.save_credentials()
+		credential_exists = Credential.find_by_site_name('Twitter')
+		self.assertEqual(credential_exists,twitter)
+
+	
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
 
 
 
-if __name__ ==  '__main__':
-    unittest.main()
+
+
+
+
+
+
+#     # def test_save_multiple_user(self):
+#     #         '''
+#     #         test_save_multiple_user to check if we can save multiple user
+#     #         objects to our user_list
+#     #         '''
+#     #         self.new_user.save_user()
+#     #         test_user = User("Test","user","0712345678") # new contact
+#     #         test_user.save_user()
+#     #         self.assertEqual(len(User.user_list),2)
+
+#     def test_delete_user(self):
+#             '''
+#             test_delete_user to test if we can remove a user from our user list
+#             '''
+#             self.new_user.save_user()
+#             test_user = User("Test","user","0712345678") # new user
+#             test_user.save_user()
+
+#             self.new_user.delete_user()# Deleting a user object
+#             self.assertEqual(len(User.user_list),1)
+
+
+#     # def test_find_user_by_first_name(self):
+#     #     '''
+#     #     test to check if we can find a user by phone number and display information
+#     #     '''
+
+#     #     self.new_user.save_user()
+#     #     test_user = User("Test","user","0712345678") # new user
+#     #     test_user.save_user()
+
+#     #     found_user = User.find_by_first_name("Test")
+
+#     #     self.assertEqual(found_user.password,test_user.password)
+
+ 
+
+#     def test_display_all_users(self):
+#         '''
+#         method that returns a list of all users saved
+#         '''
+
+#         self.assertEqual(User.display_users(),User.user_list)
+
+ 
+
+
+# class TestCredentials(unittest.TestCase):
+#     '''
+#     test class that defines a test cases dor the credentials class behaviors
+    
+#     Args: 
+#     unittest.TestCase: helps in creating test cases
+#     '''
+    
+#     def  test_check_user(self):
+#         '''
+#         function to test whether the login in function checuser woks as expected
+#         '''
+
+#         self.new_user= User('Mimi','mireille','pswd100')
+#         self.new_user.save_user()
+#         user2 = User('fanny','mireille,'pswd100')
+#         user2.save_user()
+        
+#         for user in User.user_list:
+#             if user.first_name ==  user2.first_name and user.password == user2.password:
+#                 current_user = user.first_name
+#         return current_user
+        
+#         self.assertEqual(current_user,Credential.check_user(user2.first_name,user2.password))
+    
+#     def setUp(self):
+
+#         '''
+#         function to create an account credentials beforre each test
+#         '''
+#         self.new_credential = Credential('Mireille','Facebook','Mireille_Uwase','pswd100')
+        
+#     def test__init__(self):
+#         '''
+# 		Test to if check the initialization/creation of credential instances is properly done
+# 		'''
+        
+#         self.assertEqual(self.new_credential.user_name,'Mireille')
+#         self.assertEqual(self.new_credential.site_name,'Facebook')
+#         self.assertEqual(self.new_credential.account_name,'Mireille_Uwase')
+#         self.assertEqual(self.new_credential.password,'pswd100')
+        
+#     def test_save_credentials(self):
+        
+#         '''
+# 		Test to check if the new credential info is saved into the credentials list
+# 		'''
+        
+#         self.new_credential.save_credentials()
+        
+#         facebook = Credential('mireille','Facebook','Mireille_Uwase','0727077616')
+#         facebook.save_credentials()
+#         self.assertEqual(len(Credential.credentials_list),2)
+
+#     def test_delete_credential(self):
+#             '''
+#             test_delete_user to test if we can remove a user from our user list
+#             '''
+#             self.new_credential.save_credentials()
+#             test_credential = Credential("Test","0712345678") # new user
+#             test_credential.save_credentials()
+
+#             self.new_credential.delete_credential()# Deleting a user object
+#             self.assertEqual(len(Credential.credential_list),1)
+
+#     def tearDown(self):
+        
+#         '''
+# 		Function to clear the credentials list after every test
+# 		'''
+        
+#         Credential.credentials_list = []
+#         User.users_list = []
+        
+#     def test_display_credentials(self):
+        
+#         '''
+# 		Test to check if the display_credentials method, displays the correct credentials.
+# 		'''
+        
+#         self.new_credential.save_credentials()
+#         facebook = Credential('mireille','Facebook','Mireille_Uwase','0727077616')
+#         facebook.save_credentials()
+#         github = Credential('mireille','github','Mireille95','Mimi@2019')
+#         github.save_credentials()
+        
+#         self.assertEqual(len(Credential.display_credentials(facebook.user_name)),2)
+        
+#     # def test_find_by_site_name(self):
+#     #     '''
+# 	# 	Test to check if the find_by_site_name method returns the correct credential
+# 	# 	'''
+        
+#     #     self.new_credential.save_credentials()
+
+#     #     twitter = Credential('Jane','Twitter','maryjoe','pswd100')
+#     #     twitter.save_credentials()
+#     #     credential_exists = Credential.find_by_site_name('Twitter')
+#     #     self.assertEqual(credential_exists,twitter)
+        
+#     # def test_copy_credential(self):
+        
+#     #     '''
+# 	# 	Test to check if the copy a credential method copies the correct credential
+# 	# 	'''
+        
+#     #     self.new_credential.save_credentials()
+#     #     twitter = Credential('Jane','Twitter','maryjoe','pswd100')
+#     #     twitter.save_credentials()
+#     #     find_credential = None
+#     #     for credential in Credential.user_credentials_list:
+#     #         find_credential =Credential.find_by_site_name(credential.site_name)
+#     #     return pyperclip.copy(find_credential.password)
+        
+#     #     Credential.copy_credential(self.new_credential.site_name)
+#     #     self.assertEqual('pswd100',pyperclip.paste())
+#     #     print(pyperclip.paste())
+
+
+
+# if __name__ ==  '__main__':
+#     unittest.main()
    
     
